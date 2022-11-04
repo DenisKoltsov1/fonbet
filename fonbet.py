40dc9eb765a84ca788a8008f237e792c734c8075
denis <dublin87@mail.ru>
2022-11-04 17:00:59 +0300

    request name match

--- fonbet.py
+++ fonbet.py
@@ -6,5 +6,4 @@ r = requests.get ('https://api.betting-api.com/1xbet/football/live/all', headers
 data=r.json()
 match=data[0]
 print(match['team1_rus'],'-',match['team2_rus'])
-print('-')
-print(match['team2_rus'])
+
