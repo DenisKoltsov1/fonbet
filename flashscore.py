e309b5ca0324245d77b58f8a1c5a8bd2ab6044cb
denis <dublin87@mail.ru>
2022-11-04 23:41:28 +0300

    request data with  flashscorefrom

---
+++ flashscore.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+"""
+import io.github.bonigarcia.wdm.WebDriverManager
+
+webdriver= WebDriverManager.chromedriver().setup()
+ChromeDriver driver = new ChromeDriver()
+chromedriver
+"""
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(executable_path="C:\\fonbet\\chromedriver.exe" )
+html=driver.get("https://www.flashscorekz.com/");
+time.sleep(10)
+
--- fonbet.py
+++ fonbet.py
@@ -1,9 +1,11 @@
 import requests
+import numpy as np
+from pandas import read_csv as read
 token='3d7aabee89c348b084a80ce0cadb07be1014c43716464c07ab0670fafd464c8f'
 headers = {'Authorization':token}
 import requests
 r = requests.get ('https://api.betting-api.com/1xbet/football/live/all', headers=headers)
 data=r.json()
 match=data[0]
-print(match['team1_rus'],'-',match['team2_rus'])
+print(match['team1_rus'],'-',match['team2_rus'],'  ', match['score1'],'-',match['score2'])
 
