0d1992264f91c4dbceea055f3015c4f8c31837e5
denis <dublin87@mail.ru>
2022-11-04 16:59:49 +0300

    request name match

--- fonbet.py
+++ fonbet.py
@@ -3,5 +3,8 @@ token='3d7aabee89c348b084a80ce0cadb07be1014c43716464c07ab0670fafd464c8f'
 headers = {'Authorization':token}
 import requests
 r = requests.get ('https://api.betting-api.com/1xbet/football/live/all', headers=headers)
-
-print(r.json())
+data=r.json()
+match=data[0]
+print(match['team1_rus'],'-',match['team2_rus'])
+print('-')
+print(match['team2_rus'])
