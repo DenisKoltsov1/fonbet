74f8ff7a2c9015ef9e333a956bdc07d6c5eeda04
denis <dublin87@mail.ru>
2022-11-06 19:26:48 +0300

    fetch  first team

--- flashscore.py
+++ flashscore.py
@@ -12,14 +12,14 @@ from selenium.webdriver.support import expected_conditions as EC
 # Initialize Chrome WebDriver
 driver = webdriver.Chrome(executable_path="C:\\fonbet\\chromedriver.exe" )
 chrome_options = Options()
+chrome_options.headless = True
 chrome_options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 html=driver.get("https://www.flashscorekz.com/");
 stroka=str("g_1_8QmkYkDb")
 kcal = driver.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
-for x in  kcal:
-    print(x.text)
-time.sleep(1)
+#teamHome = kcal.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
+print(kcal[0].text)
 
 
 
