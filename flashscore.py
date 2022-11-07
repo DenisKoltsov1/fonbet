eba2d224f8539264ff336df0911da36c7ce2c826
denis <dublin87@mail.ru>
2022-11-08 01:01:58 +0300

    add click to element

--- flashscore.py
+++ flashscore.py
@@ -7,6 +7,7 @@ import time
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 # Initialize Chrome WebDriver
@@ -18,8 +19,28 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 html=driver.get("https://www.flashscorekz.com/");
 stroka=str("g_1_8QmkYkDb")
 kcal = driver.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
-#teamHome = kcal.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
-print(kcal[0].text)
+time.sleep (5)
+#teamHome = driver.find_element(By.XPATH,'//*[@id="g_1_KAOyH3RR"]/div[2]').click()
+element = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))).click()
+teamHome = driver.find_element(By.XPATH,'//*[@id="g_1_KAOyH3RR"]/div[3]') 
+newPage=teamHome.click()
+#//*[@id="detail"]/div[9]/div[1]/div[3]/div
+name = driver.find_element(By.XPATH,'///*[@id="detail"]/div[9]/div[1]/div[3]/div')
+for elem in range(0,3):
+    driver.find_element(By.TAG_NAME,'body').send_keys(
+    Keys.CONTROL + "Tab")
 
+#//*[@id="detail"]/div[7]/div/a[3]  
+sostavPage = driver.find_element(By.XPATH,'//*[@id="g_1_KAOyH3RR"]/div[2]').click()   
+#//*[@id="detail"]/div[9]/div[1]/div[2]/div/div[1]/div[1]/a
+for elem in range(0,3):
+    driver.find_element(By.TAG_NAME,'body').send_keys(
+    Keys.CONTROL + "Tab")
+sostav = driver.find_elements(By.CLASS_NAME,'lf__participantName')
+for i in sostav:
+    print(i.text)
+
+print(teamHome.text)
+print(name.text)
 
 
