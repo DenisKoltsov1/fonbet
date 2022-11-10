4212137d3152d9266082cb4f7917fd1ff49bd858
denis <dublin87@mail.ru>
2022-11-11 00:47:29 +0300

    withdrawal of starting lineups

--- flashscore.py
+++ flashscore.py
@@ -17,38 +17,31 @@ chrome_options.headless = True
 chrome_options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 html=driver.get("https://www.flashscorekz.com/");
-stroka=str("g_1_8QmkYkDb")
+
 kcal = driver.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
 time.sleep (5)
 
 element = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))).click()
 live = driver.find_element(By.XPATH,'//*[@id="live-table"]/div[1]/div/div[2]')                                                     #нахождение элемента Live
 live.click()                                                                                                                       #нажатие на элемент Live
-teamHome = driver.find_element(By.XPATH,'//*[@id="g_1_C4mFUMeR"]')                                                                 #нажатие на игру для открытия игры
+teamHome = driver.find_element(By.XPATH,'//*[@id="g_1_bPRcMb37"]')                                                                 #нажатие на игру для открытия игры
 newPage=teamHome.click()
 
-handles = driver.window_handles                                                                                                    #открытие нового окна браузера с игрой
-size = len(handles)
 
-for x in range(size):                                                                                                              #переход на новое окно
-  driver.switch_to.window(handles[x])
-  print(driver.title)
 
-sostavLink=driver.find_element(By.XPATH,'//*[@id="detail"]/div[7]/div/a[3]')                                                        #безуспешная попытка нажать на вкладку "Составы"
-#sostavLink=sostavLink.get_attribute('textContent')
-#driver.find_element(By.LINK_TEXT,sostavLink).click()
-sostavLink.click()
-print(sostavLink.text)
-'''
-size1 = len(sostavLink)
-for b in  range(size1):
-    print(sostavLink[b].text)
-'''   
-#sostavTeam=driver.find_elements(By.XPATH,'//*[@id="detail"]/div[8]/div[1]/div[2]/div/div[1]/div[1]/a') 
-sostavPage = driver.find_element(By.TAG_NAME,'body') 
-#for x in sostavTeam:
-#    print(x.text) 
-print(sostavPage.text)
+handles = driver.window_handles
+driver.switch_to.window(handles[-1])
+time.sleep (5)
+sostavLink = driver.find_element(By.XPATH, '//a[.="Составы"]').click()
+time.sleep(3)
+sostavPage = driver.find_elements(By.XPATH, "//div[contains(text(), 'Стартовые составы')]/following-sibling::div[@class='lf__sidesBox']//a[@class='lf__participantName']/child::div")
+ 
+for player in sostavPage:
+    print(player.text)
+ 
+driver.quit()
+ 
+#print(sostavPage.text)
 
 
 
