d305f0866cfb987ff760b586eb9b26e027357ed5
denis <dublin87@mail.ru>
2022-11-28 02:32:37 +0300

    add command in database

--- flashscore.py
+++ flashscore.py
@@ -8,46 +8,62 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 # Initialize Chrome WebDriver
+sostavPages= []
 driver = webdriver.Chrome(executable_path="C:\\fonbet\\chromedriver.exe")
+def parser(sostavPages,driver):
+    
 
-chrome_options = Options()
-chrome_options.headless = True
-chrome_options.add_argument("--disable-extensions")
+    chrome_options = Options()
+    chrome_options.headless = True
+    chrome_options.add_argument("--disable-extensions")
 
-html=driver.get("https://www.flashscorekz.com")
-time.sleep(50)
-kcal = driver.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
-time.sleep (5)
+    html=driver.get("https://www.flashscorekz.com")
+    time.sleep(50)
+    kcal = driver.find_elements(By.XPATH,'//div[@class="event__participant event__participant--home"]')
+    time.sleep (5)
 
-element = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))).click()
-#live = driver.find_element(By.XPATH,'//div[@class="filters__group"]/div[@class="filters__tab"]/div[contains(text(),"LIVE")]') 
-#live.click() 
-HomeTeame= driver.find_element(By.XPATH,'//div[@class="event__participant event__participant--home"]')
-print(HomeTeame.text)
-AwayTeame= driver.find_element(By.XPATH,'//div[@class="event__participant event__participant--away"]')
-print(AwayTeame.text)
-#HomeMatch= driver.find_element(By.XPATH,'//div[@title="Подробности матча!"]')  
-HomeMatch= driver.find_element(By.XPATH,'//*[@id="g_1_ARmio3Rl"]')                                                             #нажатие на игру для открытия игры
-newPage=HomeMatch.click()
+    element = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.ID, 'onetrust-accept-btn-handler'))).click()
+    live = driver.find_element(By.XPATH,'//div[@class="filters__group"]/div[@class="filters__tab"]/div[contains(text(),"LIVE")]') 
+    live.click() 
+    HomeTeame= driver.find_element(By.XPATH,'//div[@class="event__participant event__participant--home"]')
+    print(HomeTeame.text)
+    AwayTeame= driver.find_element(By.XPATH,'//div[@class="event__participant event__participant--away"]')
+    print(AwayTeame.text)
+    HomeMatch= driver.find_element(By.XPATH,'//div[@title="Подробности матча!"]')  
+    #HomeMatch= driver.find_element(By.XPATH,"//section[@class='event mobile']/div[@class='event__match event__match--live event__match--twoLine']")                                                            #нажатие на игру для открытия игры
+    newPage=HomeMatch.click()
 
-handles = driver.window_handles
-driver.switch_to.window(handles[-1])
-time.sleep (5)
-#sostavLink = driver.find_element(By.XPATH, '//a[.="Составы"]').click()
-time.sleep(3)
-#sostavPage = driver.find_elements(By.XPATH, "//div[contains(text(), 'Стартовые составы')]/following-sibling::div[@class='lf__sidesBox']//a[@class='lf__participantName']/child::div")
- 
-#for player in sostavPage:
-    #print(player.text)
- 
-HistoryGame = driver.find_element(By.XPATH, '//div[@class="tabs__group"]/a[3]')
-time.sleep(5)
-HistoryGame.click()
+    handles = driver.window_handles
+    driver.switch_to.window(handles[-1])
+    time.sleep (5)
+    sostavLink = driver.find_element(By.XPATH, '//a[.="Составы"]').click()
+    time.sleep(3)
+    sostavPage = driver.find_elements(By.XPATH, "//div[contains(text(), 'Стартовые составы')]/following-sibling::div[@class='lf__sidesBox']//a[@class='lf__participantName']/child::div")
+    for player in sostavPage:
+        sostavPages.append(player)
+
+    #print(sostavPage.count())   
+    time.sleep(5)
+
+    #HomeLogo =driver.find_element(By.XPATH, '//div[@class="fixedHeaderDuel__homeLogo "]')  # ElementClickInterceptedException
+    #HomeLogo.click()
+
+    #HistoryGame = driver.find_element(By.XPATH, '//div[@class="tabs__group"]/a[3]')
+    
+    #HistoryGame.click()
+
+    #ListStitistic = driver.find_element(By.XPATH, '//div[@class="rows"]')
+
+    #print(ListStitistic.text)
+
+    
+    return sostavPages
 
 
-time.sleep(5)
-ListStitistic = driver.find_element(By.XPATH, '//div[@class="rows"]')
-
-print(ListStitistic.text)
-driver.close()
-driver.quit()
+parser(sostavPages,driver)
+#print(sostavPage.count())
+#for player in sostavPages:
+#    print(player.text)
+returnSostavPage=sostavPages
+#driver.close()
+#driver.quit()
--- sqlite.py
+++ sqlite.py
@@ -1,43 +1,39 @@
 import  sqlite3
-from  flashscore import  sostavPage
+from  flashscore import  returnSostavPage
 try:
     sqlite_connect = sqlite3.connect('C:\\fonbet\\fonbet.sqlite')
     
     
-    if """create table  FonbetTeam_Lineups_Home""":
-        print(" table  FonbetTeam_Lineups_Home creating")
-    else:
-        sqlite_create_table_query = '''CREATE TABLE FonbetTeam_Lineups_Home (
-                                    id INTEGER PRIMARY KEY  AUTO_INCREMENT,
-                                    StartTeam TEXT NOT NULL,
-                                    SpareTeam text NOT NULL UNIQUE);'''
 
-        sqlite_create_table_query2 = '''CREATE TABLE FonbetTeam_Lineups_Away (
-                                    id INTEGER PRIMARY KEY  AUTO_INCREMENT,
-                                    StartTeam TEXT NOT NULL,
-                                    SpareTeam text NOT NULL UNIQUE);'''
-        cursor=sqlite_connect.cursor()
-        cursor.execute(sqlite_create_table_query)
-        cursor.execute(sqlite_create_table_query2)
+    sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS FonbetTeam_Lineups_Home (
+                                    id INTEGER PRIMARY KEY  ,
+                                    StartTeam TEXT ,
+                                    SpareTeam TEXT )'''
 
+    sqlite_create_table_query2 = '''CREATE TABLE IF NOT EXISTS FonbetTeam_Lineups_Away (
+                                    id INTEGER PRIMARY KEY ,
+                                    StartTeam TEXT ,
+                                    SpareTeam Text  UNIQUE)'''
     cursor=sqlite_connect.cursor()
-    for player in sostavPage:
-        print(player.text)
-        sqlite_insert_query = """INSERT INTO FonbetTeam_Lineups_Home
-                            (StartTeam)  VALUES (?) """ 
-        cursor.execute(sqlite_insert_query,player)
+    cursor.execute(sqlite_create_table_query)
+    cursor.execute(sqlite_create_table_query2)
+    sqlite_connect.commit()
+    cursor=sqlite_connect.cursor()
     
+    cursor.execute("DELETE FROM FonbetTeam_Lineups_Home")
+    sqlite_connect.commit()
+    #print(dir(player))
     
-    
-
-
-    
-    
-
-    
-
-    cursor.execute(sqlite_insert_query,(player))
-    sqlite_connect.commit
+    playerRext='Denis'
+    #print(player.text)
+    smtp='INSERT INTO FonbetTeam_Lineups_Home (StartTeam)  VALUES (?)' 
+    for player in returnSostavPage:
+        
+        #print(player.text)
+        
+        cursor.execute('INSERT INTO FonbetTeam_Lineups_Home (StartTeam)  VALUES (?)' ,(player.text,))
+       
+    sqlite_connect.commit()
     print("Таблица SQLite создана")
     
     cursor.close()
@@ -46,6 +42,8 @@ except sqlite3.Error as error:
     print("Ошибка при подключении к sqlite", error)
 finally:
     if (sqlite_connect):
+        cursor.close()
         sqlite_connect.close()
+       
         print("Соединение с SQLite закрыто")
 
